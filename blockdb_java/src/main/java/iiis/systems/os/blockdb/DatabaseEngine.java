2281ae9ca00d7e50207c3225344b40fb20812055
ACMonsterQiao@gmail.com <ACMonsterQiao@gmail.com>
2018-05-23 19:31:45 +0800

    now transactions are written to a temporary log

--- blockdb_java/src/main/java/iiis/systems/os/blockdb/DatabaseEngine.java
+++ blockdb_java/src/main/java/iiis/systems/os/blockdb/DatabaseEngine.java
@@ -2,8 +2,15 @@ package iiis.systems.os.blockdb;
 
 import java.util.HashMap;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.IOException;
+
 public class DatabaseEngine {
     private static DatabaseEngine instance = null;
+
+    static final String logFileName = "log.txt";
 
     public static DatabaseEngine getInstance() {
         return instance;
@@ -19,6 +26,13 @@ public class DatabaseEngine {
 
     DatabaseEngine(String dataDir) {
         this.dataDir = dataDir;
+        JSONObject logObject = Util.readJsonFile(dataDir + logFileName);
+        if (logObject == null) {
+        	logObject = new JSONObject();
+        	logObject.put("numBlocks", 0);
+        	logObject.put("Transactions", new JSONArray());
+        	Util.writeJsonFile(dataDir + logFileName, logObject);
+        }
     }
 
     private int getOrZero(String userId) {
@@ -29,19 +43,52 @@ public class DatabaseEngine {
         }
     }
 
+    private void addTransaction(int op, String userID, String fromID, String toID, int value) {
+    	JSONObject transaction = new JSONObject();
+    	switch (op) {
+    		case putOp:
+    			transaction.put("Type", "PUT");
+    			transaction.put("UserID", userID);
+    			transaction.put("Value", value);
+
+    		case depositOp:
+    			transaction.put("Type", "DEPOSIT");
+    			transaction.put("UserID", userID);
+    			transaction.put("Value", value);
+
+    		case withdrawOp:
+    			transaction.put("Type", "WITHDRAW");
+    			transaction.put("UserID", userID);
+    			transaction.put("Value", value);
+
+    		case transferOp:
+    			transaction.put("Type", "TRANSFER");
+    			transaction.put("FromID", fromID);
+    			transaction.put("ToID", toID);
+    			transaction.put("Value", value);
+
+    		default:
+    			System.out.println("ERROR: UNKNOWN TRANSACTION TYPE!");
+    	}
+    	JSONObject logObject = Util.readJsonFile(dataDir + logFileName);
+    	logObject.getJSONArray("Transactions").put(transaction);
+    	Util.writeJsonFile(dataDir + logFileName, logObject);
+    }
+
     public int get(String userId) {
-        logLength++;
         return getOrZero(userId);
     }
 
     public boolean put(String userId, int value) {
         logLength++;
+        addTransaction(putOp, userId, null, null, value);
         balances.put(userId, value);
         return true;
     }
 
     public boolean deposit(String userId, int value) {
         logLength++;
+        addTransaction(depositOp, userId, null, null, value);
         int balance = getOrZero(userId);
         balances.put(userId, balance + value);
         return true;
@@ -50,6 +97,7 @@ public class DatabaseEngine {
     public boolean withdraw(String userId, int value) {
         logLength++;
         int balance = getOrZero(userId);
+        addTransaction(withdrawOp, userId, null, null, value);
         balances.put(userId, balance - value);
         return true;
     }
@@ -58,6 +106,7 @@ public class DatabaseEngine {
         logLength++;
         int fromBalance = getOrZero(fromId);
         int toBalance = getOrZero(toId);
+        addTransaction(transferOp, null, fromId, toId, value);
         balances.put(fromId, fromBalance - value);
         balances.put(toId, toBalance + value);
         return true;
@@ -66,4 +115,9 @@ public class DatabaseEngine {
     public int getLogLength() {
         return logLength;
     }
+
+    private final int putOp = 1;
+    private final int depositOp = 2;
+    private final int withdrawOp = 3;
+    private final int transferOp = 4;
 }
--- blockdb_java/src/main/java/iiis/systems/os/blockdb/Util.java
+++ blockdb_java/src/main/java/iiis/systems/os/blockdb/Util.java
@@ -7,8 +7,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Util {
-    public static JSONObject readJsonFile(String filePath) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(filePath)));
-        return new JSONObject(content);
+    public static JSONObject readJsonFile(String filePath) {
+    	try {
+    		String content = new String(Files.readAllBytes(Paths.get(filePath)));
+    		return new JSONObject(content);	
+    	} catch (IOException e) {
+    		return null;
+    	}
+    }
+
+    public static boolean writeJsonFile(String filePath, JSONObject o) {
+    	boolean success = true;
+    	try {
+    		Files.write(Paths.get(filePath), o.toString().getBytes());
+    	} catch (IOException e) {
+    		success = false;
+    	}
+    	return success;
     }
 }
