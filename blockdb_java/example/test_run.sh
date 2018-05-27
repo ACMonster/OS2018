071068c449a5e92ce778bfc8077af7340a6b9461
LeoYu <yudingli_good@126.com>
2018-05-27 13:05:09 -0400

    Java Client and new test_run.sh

--- blockdb_java/example/test_run.sh
+++ blockdb_java/example/test_run.sh
@@ -23,58 +23,58 @@ sleep 1
 
 echo "Step 1: Initialize account"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient PUT TEST---$I 10
+	go run ./example/test_client.go -T=PUT -user=TEST---$I -value=10
 done
 echo "Check value: expecting value=10"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---5
+go run ./example/test_client.go -T=GET -user=TEST---5
 
 echo "Check LogLength: expecting value=10"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient LOGLENGTH
+go run ./example/test_client.go -T=LogLength
 
 echo "Step 2: Try deposit"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient DEPOSIT TEST---$I 5
+	go run ./example/test_client.go -T=DEPOSIT -user=TEST---$I -value=5
 done
 echo "Check value: expecting value=15"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---5
+go run ./example/test_client.go -T=GET -user=TEST---5
 
 echo "Step 3: Try transfer"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient TRANSFER TEST---$I TEST---TX 10
+	go run ./example/test_client.go -T=TRANSFER -from=TEST---$I -to=TEST--TX  -value=10
 done
 echo "Check value: expecting value=100"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---TX
+go run ./example/test_client.go -T=GET -user=TEST--TX
 
 echo "Step 4: Try transfer again"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient TRANSFER TEST---TX TEST---$I 5
+	go run ./example/test_client.go -T=TRANSFER -from=TEST--TX -to=TEST---$I  -value=5
 done
 echo "Check value: expecting value=50"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---TX
+go run ./example/test_client.go -T=GET -user=TEST--TX
 
 echo "Step 5: Try withdraw"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient WITHDRAW TEST---$I 5
+	go run ./example/test_client.go -T=WITHDRAW -user=TEST---$I  -value=5
 done
 echo "Check value: expecting value=5"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---2
+go run ./example/test_client.go -T=GET -user=TEST---2
 
 echo "Step 5: Try overdraft"
 for I in `seq 0 9`; do
-	java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient WITHDRAW TEST---$I 10
+	go run ./example/test_client.go -T=WITHDRAW -user=TEST---$I  -value=10
 done
 echo "Check value: expecting value=5"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---2
+go run ./example/test_client.go -T=GET -user=TEST---2
 
 echo "Check LogLength: expecting value<=50"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient LOGLENGTH
+go run ./example/test_client.go -T=LogLength
 
 #echo "Try Killing the server and restart..."
 #echo "Sleep for a while, waiting for hashmap reconstruction..."
 #sleep 10;
 
 echo "Check value again: expecting value=5"
-java -cp target/blockdb-1.0-SNAPSHOT.jar iiis.systems.os.blockdb.DatabaseClient GET TEST---2
+go run ./example/test_client.go -T=GET -user=TEST---2
 
 
 echo "Test completed. Please verify JSON block output with example_1.json ."
